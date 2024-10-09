fa2853e2d71667b7c5e0e4f474bb3f3cb0d1a4a9
SatriaBelva <satriabelvanararya@gmail.com>
2024-10-09 12:07:30 +0700

    Belajar Map

---
+++ bin/map.dart
@@ -0,0 +1,22 @@
+void main(){
+    Map <String, String> mapPertama = {
+        "nama": "Satria Belva Nararya",
+        "Divisi" : "Kaderisasi",
+        "Alamat" : "Jl. Raya Kedungwungu",
+    };
+    print(mapPertama);
+
+    var mapKedua = <String, String> {
+        'Nama' : 'Bejo',
+        'Divisi' : 'Kaderisasi',
+        'Alamat' : 'Jl. Raya Kedungwungu'
+    };
+    print(mapKedua);
+
+    var mapKetiga = {
+        'nama' : 'Dewi',
+        'divisi' : 'PSDM',
+        'Alamat' : 'Probolinggo'
+    };
+    print(mapKetiga);
+}
--- bin/set.dart
+++ bin/set.dart
@@ -1,7 +1,26 @@
 void main(){
-    Set <String> setPertama = {"Bejo", "Bejo", "Satria", "Belva", 'Nararya'};
-    var setKedua = <int> {1,2,3,4,5,6,6};
+    
+    Set <String> setPertama = {"Bejo", "Bejo", "Satria", "Belva", 'Nararya', 'Kaderisasi'};
+    print("Apakah Mengandung Kaderisasi : ${setPertama.contains("Kaderisasi")}");
+    print("Apakah Mengandung HIMASIF    : ${setPertama.contains("HIMASIF")}");
 
-    print(setPertama);
-    print(setKedua);
+    // setPertama.remove("Satria");
+    // print(setPertama);
+
+    // setPertama.removeAll(['Bejo', 'Tambahan']);
+    // print(setPertama);
+    // setPertama.add("Tambahan");
+    // print(setPertama);
+    // var tes = setPertama.length;
+    // print("Panjang set nya adalah $tes")
+
+    // var setKedua = <int> {1,2,3,4,5,6,6};
+    // print(setKedua);
+
+    // Set <int> setKetiga = {5,1,8,7};
+    // print(setKetiga);
+
+    // var setKeempat = {1,5,2,6,5,9};
+    // print(setKeempat);
+
 }
